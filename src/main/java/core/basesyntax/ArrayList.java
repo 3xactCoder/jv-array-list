9489e67e5840f6a72bec943bfefe5e02c57121f5
3xacT <vovaoliynyk99@gmail.com>
2026-02-06 16:26:06 +0200

    created simplified custom array-list

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,103 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_SIZE = 10;
+    private int count;
+    private Object[] elements;
+
+    public ArrayList() {
+        elements = new Object[DEFAULT_SIZE];
+        count = 0;
+
+    }
+
     @Override
     public void add(T value) {
+        checkCapacity();
+        elements[count++] = value;
 
     }
 
     @Override
     public void add(T value, int index) {
+        if (index < 0 || index > count) {
+            throw new ArrayListIndexOutOfBoundsException("Index is incorrect");
+        }
+        checkCapacity();
+        System.arraycopy(elements, index, elements, index + 1, count - index);
+        elements[index] = value;
+        count++;
 
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0;i < list.size();i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        return (T) elements[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIndex(index);
+        elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+        T remove = (T) elements[index];
+        System.arraycopy(elements,index + 1,elements,index,count - index - 1);
+        elements[--count] = null;
+        return remove;
     }
 
     @Override
-    public T remove(T element) {
-        return null;
+    public T remove(T element) throws NoSuchElementException {
+        for (int i = 0; i < count; i++) {
+            if (element == null) {
+                if (elements[i] == null) {
+                    return remove(i);
+                }
+            } else {
+                if (element.equals(elements[i])) {
+                    return remove(i);
+                }
+            }
+        }
+        throw new NoSuchElementException("Non existing element");
     }
 
     @Override
     public int size() {
-        return 0;
+        return count;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return count == 0;
     }
+
+    public void checkIndex(int index) {
+        if (index < 0 || index >= count) {
+            throw new ArrayListIndexOutOfBoundsException("Index is incorrect");
+        }
+    }
+
+    public void checkCapacity() {
+        if (count == elements.length) {
+            int newSize = elements.length + elements.length / 2;
+            elements = Arrays.copyOf(elements,newSize);
+        }
+    }
+
 }
